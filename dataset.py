0fa75465e8b015f7eb845dc63066c4544b252112
leni <leni.allard@ensta-bretagne.org>
2023-01-18 10:13:11 +0100

    Delete ImageDataSet class

--- dataset.py
+++ dataset.py
@@ -1,21 +1,8 @@
 import torchvision
 import torchvision.transforms as transforms
-
-import glob
 import numpy as np
-
 import torch
-from torch.utils.data import Dataset
-from PIL import Image
 
-transform = transforms.Compose(
-    [transforms.ToTensor()])
-
-trainset = torchvision.datasets.CIFAR10(root='data/', train=True,
-                                        download=True, transform=transform)
-
-testset = torchvision.datasets.CIFAR10(root='data/', train=False,
-                                       download=True, transform=transform)
 
 # Normalization parameters for pre-trained PyTorch models
 mean = np.array([0.485, 0.456, 0.406])
@@ -27,38 +14,6 @@ def denormalize(tensors):
     for c in range(3):
         tensors[:, c].mul_(std[c]).add_(mean[c])
     return torch.clamp(tensors, 0, 255)
-
-
-class ImageDataset(Dataset):
-    def __init__(self, root, hr_shape):
-        hr_height, hr_width = hr_shape
-        # Transforms for low resolution images and high resolution images
-        self.lr_transform = transforms.Compose(
-            [
-                transforms.Resize((hr_height // 4, hr_height // 4), transforms.InterpolationMode.BICUBIC),
-                transforms.ToTensor(),
-                transforms.Normalize(mean, std),
-            ]
-        )
-        self.hr_transform = transforms.Compose(
-            [
-                transforms.Resize((hr_height, hr_height), transforms.InterpolationMode.BICUBIC),
-                transforms.ToTensor(),
-                transforms.Normalize(mean, std),
-            ]
-        )
-
-        self.files = sorted(glob.glob(root + "/*.*"))
-
-    def __getitem__(self, index):
-        img = Image.open(self.files[index % len(self.files)])
-        img_lr = self.lr_transform(img)
-        img_hr = self.hr_transform(img)
-
-        return {"lr": img_lr, "hr": img_hr}
-
-    def __len__(self):
-        return len(self.files)
 
 
 class Cifar10Mod(torchvision.datasets.CIFAR10):
@@ -82,7 +37,7 @@ class Cifar10Mod(torchvision.datasets.CIFAR10):
         )
 
     def __getitem__(self, item):
-        image, target = super().__getitem__(item)
+        image, _ = super().__getitem__(item)
         img_lr = self.lr_transform(image)
         img_hr = self.hr_transform(image)
 
